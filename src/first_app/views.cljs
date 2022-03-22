34798fcc4e11437c4fc277a80891e3b5f81f5d40
ohanlines <rauhanhouse7@gmail.com>
2022-03-22 22:18:40 +0700

    Added text box and button

--- src/first_app/views.cljs
+++ src/first_app/views.cljs
@@ -4,9 +4,15 @@
    [first-app.subs :as subs]
    ))
 
+(defn text-input []
+  [:div.field.has-addons
+   [:div.control
+    [:input {:type "text" :placeholder "Text Input"}]]
+   [:div.control
+    [:button "Insert"]]])
+
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1
-      "Hello from " @name]
+    [:div.section
+     [text-input]
      ]))
