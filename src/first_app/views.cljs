1525b4bd525526deefa8940a7c0857e2ba202f82
ohanlines <rauhanhouse7@gmail.com>
2022-03-23 10:57:57 +0700

    Added text-list function
    text-list have a function to show text inside text box after insert button clicked

--- src/first_app/views.cljs
+++ src/first_app/views.cljs
@@ -1,18 +1,26 @@
 (ns first-app.views
   (:require
    [re-frame.core :as re-frame]
-   [first-app.subs :as subs]
-   ))
+   [first-app.events :as events]
+   [first-app.subs :as subs]))
 
-(defn text-input []
-  [:div.field.has-addons
-   [:div.control
-    [:input {:type "text" :placeholder "Text Input"}]]
-   [:div.control
-    [:button "Insert"]]])
+(defn text-input [id]
+  (let [value (re-frame/subscribe [::subs/text-subs id])]
+    [:div.field.has-addons
+     [:div.control
+      [:input {:value @value
+               :on-change #(re-frame/dispatch [::events/update-text id (-> % .-target .-value)])
+               :type "text" :placeholder "Text Input"}]
+      [:button {:on-click #(re-frame/dispatch [::events/insert-text])} "Insert"]]]))
+
+(defn text-list []
+  (let [text @(re-frame/subscribe [::subs/list-subs])]
+    [:div
+     [:ul (map (fn [s] [:li (str s)]) text)]]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div.section
-     [text-input]
+     [text-input :text-input]
+     [text-list]
      ]))
