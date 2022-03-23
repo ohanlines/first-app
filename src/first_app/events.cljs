f35470bd4268acdceccbf48efc3b213216da681f
ohanlines <rauhanhouse7@gmail.com>
2022-03-23 10:54:03 +0700

    Added ::update-text and ::insert-text
    ::update-text for update text inside db, ::insert-text for insert updated text into new db

--- src/first_app/events.cljs
+++ src/first_app/events.cljs
@@ -1,10 +1,24 @@
 (ns first-app.events
   (:require
    [re-frame.core :as re-frame]
-   [first-app.db :as db]
-   ))
+   [first-app.db :as db]))
 
 (re-frame/reg-event-db
  ::initialize-db
  (fn [_ _]
    db/default-db))
+
+(re-frame/reg-event-db
+ ::update-text
+ (fn [db [_ key val]]
+   (assoc db key val)))
+
+(re-frame/reg-event-db
+ ::insert-text
+ (fn [db]
+   (let [data    (get db :text-input)
+         text    (get db :text [])
+         updated (conj text data)]
+     (-> db
+         (assoc :text updated)
+         (dissoc :text-input)))))
