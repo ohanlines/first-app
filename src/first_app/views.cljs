2a63ad1f8e6344d2ece30be7c5eb35a4e6ce50d8
ohanlines <rauhanhouse7@gmail.com>
2022-04-05 13:25:15 +0700

    change list format

--- src/first_app/views.cljs
+++ src/first_app/views.cljs
@@ -8,22 +8,18 @@
   (let [value (re-frame/subscribe [::subs/text-subs id])]
     [:div.field.has-addons
      [:div.control
-      [:input {:value @value
-               :on-change #(re-frame/dispatch [::events/update-text id (-> % .-target .-value)])
-               :type "text" :placeholder "Text Input"}]
-      [:button {:on-click #(re-frame/dispatch [::events/insert-text])} "Insert"]]]))
-
-(defn text-list []
-  (let [text @(re-frame/subscribe [::subs/list-subs])]
-    [:div
-     [:ul (map (fn [s] [:li s]) text)]]))
+      [:input {:value       @value
+               :type        "text"
+               :placeholder "Text Input"
+               :on-change   #(re-frame/dispatch [::events/update-text id (-> % .-target .-value)])}]
+      [:button {:disabled   (zero? (count @value))
+                :on-click   #(do (re-frame/dispatch [::events/insert-text])
+                                 (re-frame/dispatch [::events/dissoc-text]))} "Insert"]
+      [:button {:on-click   #(re-frame/dispatch [::events/clear-storage])} "Clear"]]]))
 
 (defn main-panel []
-  (let [list  @(re-frame/subscribe [::subs/list-subs])
-        names @(re-frame/subscribe [::subs/name])]
+  (let [list  @(re-frame/subscribe [::subs/list-subs])]
     [:div.section
-     [:h1
-      "Hello from " names]
      [text-input :text-staging]
      (map #(vector :div [:p %]) list)
      ]))
